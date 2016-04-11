792ea5fbb749410888aebffb535628a032d6464a
adityaOC <adidev70@gmail.com>
2016-04-11 16:40:06 +0530

    Update AppDelegate.m

--- jenkinsProject/AppDelegate.m
+++ jenkinsProject/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //change from git
     return YES;
 }
 
